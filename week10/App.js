aa666660759955861df637f3fec122e7cd956d8a
mperlasdig4639 <77718126+mperlasdig4639@users.noreply.github.com>
2021-03-19 19:22:32 -0400

    week10 assignment

---
+++ week10/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My name is Michael Perlas.</Text>
+      <Text>My favorite food is pizza.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
